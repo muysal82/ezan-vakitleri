dc58b6c99417276d38f67a251ccb1d1b1f836898
AFN <user@example.com>
2026-03-28 11:30:46 +0300

    fix: add base path for GitHub Pages

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/ezan-vakitleri/sw.js', { scope: '/ezan-vakitleri/' })})}
